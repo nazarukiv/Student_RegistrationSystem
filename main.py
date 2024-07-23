dec09b0e217b17237023fd9231a389472c9f7ce8
Ivan Nazaruk <nazaruk7649@ukr.net>
2024-07-23 11:12:55 +0100

    Add basic Tkinter framework and initial layout

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print('First Test')
+from tkinter import *
+from datetime import date
+from tkinter import filedialog
+from tkinter import messagebox
+from PIL import Image
+import os
+from tkinter.ttk import Combobox
+import openpyxl, xlrd
+from openpyxl import Workbook
+import pathlib
+
+background = "#06283D"
+framebg = "#EDEDED"
+framefg = "#06283D"
+
+root = Tk()
+root.title("Student Registration System")
+root.geometry("1250x700+210+100")
+root.config(bg=background)
+
+root.mainloop()
